2cfc585e509a21639fd060a2b48ed0b51b9c1e20
Lucas <metallucas.ds@gmail.com>
2013-07-04 21:38:45 -0300

    Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var path = require('path');
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+var folderMount = function folderMount(connect, point) {
+  return connect.static(path.resolve(point));
+};
+
+module.exports = function(grunt) {
+
+  //  Project configuration
+  grunt.initConfig({
+
+    //  Read the package.json
+    pkg: grunt.file.readJSON('package.json'),
+
+    //  Metadata
+
+    meta: {
+      srcPath: 'sass/',
+      deployPath: 'css/'
+    },
+
+    //  Task configuration
+    sass: {
+      dist: {
+        files: {
+          '<%= meta.deployPath %>style.css' : '<%= meta.srcPath %>style.scss'
+        },
+        options: {
+          style: 'compressed'
+        }
+      }
+    },
+
+    livereload: {
+      port: 35729
+    },
+
+    connect: {
+      livereload: {
+        options: {
+          port: 9001,
+          middleware: function(connect, options) {
+            return [lrSnippet, folderMount(connect, options.base)]
+          }
+        }
+      }
+    },
+
+    regarde: {
+      watch: {
+        files: ['<%= meta.srcPath %>*.scss', '*.html'],
+        tasks: ['sass', 'livereload'],
+        events: true
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+
+  grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
+};
