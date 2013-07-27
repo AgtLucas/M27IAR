da68e5d6459b2eed883071eec0c0c04a36d84b9d
Lucas <metallucas.ds@gmail.com>
2013-07-27 19:43:23 -0300

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
           '<%= meta.jsPath %>main.min.js' : ['<%= meta.jsPath %>main.js']
         }
       }
-    }
+    },
 
     livereload: {
       port: 35729
